e52b940cfca8ea1d5a76440abd810618424ac24c
rakbidb <rakbidb@gmail.com>
2023-10-27 00:33:32 +0700

    checkpoint filter berdasarkan judul dengan dummy data

--- main/models.py
+++ main/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Product(models.Model):
     title = models.TextField(null=True, blank=True)
     language = models.TextField(null=True, blank=True)
