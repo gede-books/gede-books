e300aad0892603328be3553c995f13e6f6440edc
divieurora <divieurora@gmail.com>
2023-10-27 22:47:52 +0700

    Menambahkan form authentication

--- main/models.py
+++ main/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.TextField(null=True, blank=True)
@@ -13,3 +11,18 @@ class Product(models.Model):
     category = models.TextField(null=True, blank=True)
     stock = models.IntegerField(default=0)
     price = models.FloatField(default=0)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    
+    def get_total(self):
+        return sum(item.get_total_price() for item in self.orderitem_set.all())
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def get_total_price(self):
+        return self.product.price * self.quantity
