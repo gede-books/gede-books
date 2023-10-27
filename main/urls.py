69bae46e19060c0000380b8294cf2a0fcf6d8cd3
divieurora <divieurora@gmail.com>
2023-10-27 23:59:00 +0700

    Menambahkan routing & button HTML

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, register, login_user, logout
+from main.views import show_main, register, login_user, logout, add_to_cart, remove_from_cart, cart_view
 
 app_name = 'main'
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout, name='logout'),
+    path('add_to_cart/', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/', remove_from_cart, name='remove_from_cart'),
+    path('cart/', cart_view, name='cart_view'),
 ]
