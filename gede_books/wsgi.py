f73b573aa423e4faaa01c3ef0603319623cd499c
adhan-857 <adhan.andika857@gmail.com>
2023-11-26 16:21:31 +0700

    Coba re-deploy (1/4 modul)

--- gede_books/wsgi.py
+++ gede_books/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gede_books.settings')
 
 application = get_wsgi_application()
+
+app = application
