4fc2a9d6f265e0b513f3957c1b73d65455bbaf14
adhan-857 <adhan.andika857@gmail.com>
2023-11-26 14:11:20 +0700

    Memperbaiki agar bisa deploy (1/4 modul)

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from main.views import show_main, register, product_details, login_user, logout_user, add_to_cart, remove_from_cart, cart_view, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, show_guest, register, product_details, login_user, logout_user, add_to_cart, remove_from_cart, cart_view, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('main/', show_main, name='show_main'),
+    path('', show_guest, name='show_guest'),
     path('register/', register, name='register'),
     path('product/<int:product_id>/', product_details, name='product_details'),
     path('login/', login_user, name='login'),
