936c0b891208663d050e56e1e2f1dc01d9359ee5
rakbidb <rakbidb@gmail.com>
2023-12-18 18:13:14 +0700

    deployment

--- gede_books/settings.py
+++ gede_books/settings.py
@@ -33,8 +33,8 @@ PRODUCTION = env.bool('PRODUCTION', False)
 DEBUG = True
 
 ALLOWED_HOSTS = ["gedebooks-a07-tk.pbp.cs.ui.ac.id",
-                 "https://gedebooks-a07-tk.pbp.cs.ui.ac.id",
                  "http://gedebooks-a07-tk.pbp.cs.ui.ac.id",
+                 "https://gedebooks-a07-tk.pbp.cs.ui.ac.id",
                  "https://gede-books-a07-tk-pbp.35.209.250.177.sslip.io",
                  "localhost"]
 
